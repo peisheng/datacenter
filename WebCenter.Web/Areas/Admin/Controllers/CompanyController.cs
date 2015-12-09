9923db2293e2348415feda60ccd24c781152ab47
ilovemeyou2000 <ilovemeyou2000@163.com>
2015-12-10 01:29:41 +0800

    梦想

---
+++ WebCenter.Web/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,110 @@
+﻿using Common;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using WebCenter.Entities;
+using WebCenter.IServices;
+using WebCenter.Web.Controllers;
+
+
+namespace WebCenter.Web.Areas.Admin.Controllers
+{
+    public class CompanyController : BaseController
+    {
+        public CompanyController(IUnitOfWork UOF)
+            : base(UOF)
+        {
+
+        }
+
+        /// <summary>
+        ///     company list
+        /// </summary>
+        /// <param name="page_index"></param>
+        /// <param name="page_size"></param>
+        /// <param name="keyword"></param>
+        /// <returns></returns>
+        public ActionResult List(int page_index, int page_size, string keyword = "")
+        {
+            Expression<Func<company, bool>> condition = com => true;
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                Expression<Func<company, bool>> tmp = com => com.name.Contains(keyword);
+                condition = tmp;
+            }
+            PagedList<company> list = Uof.IcompanyService.GetAll(condition).OrderByDescending(item => item.Id).ToPagedList(page_index, page_size);
+
+            List<int> listId = list.Select(p => p.Id).ToList();
+            var companyList = Uof.IuserService.GetAll(item => listId.Contains(item.Id)).GroupBy(item => item.company_id).Select(item => new { companyId = item.Key, count = item.Count() }).ToList();
+            var obj = new ArrayList();
+            foreach (var item in list)
+            {
+                var it = new
+                {
+                    id = item.Id,
+                    name = item.name,
+                    type_name = item.sys_dictionary == null ? "" : item.sys_dictionary.value,
+                    city_name = item.city == null ? "" : item.city.city_name,
+                    author_name = item.user == null ? "" : item.user.real_name,
+                    view_count = 0,
+                    article_count = item.project_case1.Count,
+                    logo_path = item.logo_path,
+                    member_count = companyList.FirstOrDefault(x => x.companyId == item.Id) == null ? 0 : companyList.FirstOrDefault(x => x.companyId == item.Id).count,
+                    create_time = item.create_time
+                };
+                obj.Add(it);
+            }
+            var result = new
+            {
+                total_count = list.TotalCount,
+                current_page = page_index,
+                page_size = page_size,
+                items = obj
+            };
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+        /// <summary>
+        /// 保存公司
+        /// </summary>
+        /// <param name="com"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Save(company com)
+        {
+            company _company = null;             
+           if(com!=null)
+           {
+               _company = new company();
+               _company.address = com.address;
+               _company.city_id = com.city_id;                
+               _company.company_image_path = "";
+               _company.logo_path = com.logo_path;
+               _company.site_url = com.site_url;
+               _company.type_id = com.type_id;
+               _company.create_time = DateTime.Now;
+               _company.Id = com.Id;
+               if (_company.Id > 0)
+                   Uof.IcompanyService.UpdateEntity(_company);
+               else
+               {
+                   _company = Uof.IcompanyService.AddEntity(_company);
+               }
+
+               var obj = new
+               {
+                   company_id = _company.Id,
+                   result = true
+               };
+               return Json(obj);
+           }
+           return Json(new { result=false});
+
+        }
+ 
+    }
+}
---
+++ WebCenter.Web/Areas/Admin/Controllers/DictionaryController.cs
@@ -0,0 +1,58 @@
+﻿using Common;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using WebCenter.Entities;
+using WebCenter.IServices;
+using WebCenter.Web.Controllers;
+
+
+namespace WebCenter.Web.Areas.Admin.Controllers
+{
+    public class DictionaryController : BaseController
+    {
+        public DictionaryController(IUnitOfWork UOF)
+            : base(UOF)
+        {
+
+        }
+        public Isys_dictionaryService DictService
+        {
+            get
+            {
+                return Uof.Isys_dictionaryService;
+            }
+        }
+
+        public ActionResult Get(string type = "")
+        {
+
+            if (!string.IsNullOrEmpty(type))
+            {
+                var obj = DictService.GetAll(item => item.group == type).ToList();
+                ArrayList al = new ArrayList();
+                foreach (var item in obj)
+                {
+                    al.Add(new
+                    {
+                        id = item.id,
+                        name = item.name,
+                        value = item.value
+                    });
+                }
+                return Json(al, JsonRequestBehavior.AllowGet);
+            }
+
+            return Json(new { result = false }, JsonRequestBehavior.AllowGet);
+
+
+        }
+
+
+    }
+}
